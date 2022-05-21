252c8d0a6f6c419403724b4bca7e613bd898a8dc
雷鹏 <leip@linkingthing.com>
2022-05-21 14:29:16 +0800

    add validtor

---
+++ security/validator.go
@@ -0,0 +1,43 @@
+package security
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type NameRegexp struct {
+	Regexp       *regexp.Regexp
+	ErrMsg       string
+	ExpectResult bool
+}
+
+type CheckName interface {
+	GetNameRegexps() []*NameRegexp
+}
+
+var NameRegs = []*NameRegexp{
+	{
+		Regexp:       regexp.MustCompile(`^[0-9a-zA-Z-_/,，、\p{Han}@*]+$`),
+		ErrMsg:       "is not legal",
+		ExpectResult: true,
+	},
+	{
+		Regexp:       regexp.MustCompile(`(^-)|(^\.)|(^/)|(^,)|(^，)|(^、)|(^@)|(^*)`),
+		ErrMsg:       "is not legal",
+		ExpectResult: false,
+	},
+	{
+		Regexp:       regexp.MustCompile(`-$|_$|/$|，$|、$|,$|\.$|@$|\*$`),
+		ErrMsg:       "is not legal",
+		ExpectResult: false,
+	},
+}
+
+func CheckStringValid(s string) error {
+	for _, reg := range NameRegs {
+		if ret := reg.Regexp.MatchString(s); ret != reg.ExpectResult {
+			return fmt.Errorf("%s %s", s, reg.ErrMsg)
+		}
+	}
+	return nil
+}
