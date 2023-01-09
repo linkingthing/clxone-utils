1c176018f879a23fbef56766660653a6b742ccdc
雷鹏 <leip@linkingthing.com>
2023-01-09 16:26:33 +0800

    add excel support

--- csv/csv.go
+++ csv/csv.go
@@ -1,21 +1,26 @@
 package util
 
 import (
+	"bytes"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"path"
 	"strings"
 	"unicode"
+	"unicode/utf8"
 
 	"github.com/linkingthing/cement/slice"
-	"github.com/xuri/excelize/v2"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 )
 
 const (
 	UTF8BOM            = "\xEF\xBB\xBF"
 	TimeFormat         = "2006-01-02 15:04:05"
 	CSVFileSuffix      = ".csv"
-	ExcelFileSuffix    = ".xlsx"
 	UploadDirectoryKey = "directory"
 	UploadFileKey      = "path"
 	UploadFileName     = "filename"
@@ -78,19 +83,27 @@ func WriteCSVFile(fileName string, tableHeader []string, contents [][]string) (s
 		return "", fmt.Errorf("empty file")
 	}
 
-	file := excelize.NewFile()
-	for i, row := range append([][]string{tableHeader}, contents...) {
-		axis := fmt.Sprintf("A%d", i+1)
-		if err := file.SetSheetRow("Sheet1", axis, &row); err != nil {
-			return "", fmt.Errorf("write to excel file %s failed: %s", fileName, err.Error())
-		}
+	fileName = fileName + CSVFileSuffix
+	filepath := path.Join(FileRootPath, fileName)
+	file, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return "", fmt.Errorf("create csv file %s failed: %s", filepath, err.Error())
 	}
 
-	fileName = fileName + ExcelFileSuffix
-	if err := file.SaveAs(path.Join(FileRootPath, fileName)); err != nil {
-		return "", fmt.Errorf("write to excel file %s failed: %s", fileName, err.Error())
+	defer file.Close()
+	if _, err = file.WriteString(UTF8BOM); err != nil {
+		return "", err
+	}
+	w := csv.NewWriter(file)
+	if err := w.Write(tableHeader); err != nil {
+		return "", fmt.Errorf("write table header to csv file %s failed: %s", filepath, err.Error())
 	}
 
+	if err := w.WriteAll(contents); err != nil {
+		return "", fmt.Errorf("write data to csv file %s failed: %s", filepath, err.Error())
+	}
+
+	w.Flush()
 	return fileName, nil
 }
 
@@ -103,37 +116,35 @@ func ReadCSVFile(fileName string) ([][]string, error) {
 		return nil, fmt.Errorf("file name invalid with path traversal attacks")
 	}
 
-	file, err := excelize.OpenFile(path.Join(FileRootPath, fileName))
+	file, err := os.Open(path.Join(FileRootPath, fileName))
 	if err != nil {
-		return nil, fmt.Errorf("open excel file %s failed: %v", fileName, err)
+		return nil, err
 	}
 	defer file.Close()
 
-	rows, err := file.GetRows(file.GetSheetName(0))
+	b, err := ioutil.ReadAll(file)
 	if err != nil {
-		return nil, fmt.Errorf("read excel file %s failed: %v", fileName, err)
+		return nil, err
 	}
 
-	headerCellCount := 0
-	contents := make([][]string, 0, len(rows))
-	for _, row := range rows {
-		if len(row) == 0 {
-			continue
+	r := csv.NewReader(bytes.NewReader(b))
+	header, err := r.Read()
+	if err != nil && err != io.EOF {
+		return nil, err
+	} else if !utf8.ValidString(header[0]) {
+		r = csv.NewReader(transform.NewReader(bytes.NewReader(b), simplifiedchinese.GB18030.NewDecoder()))
+		if header, err = r.Read(); err != nil {
+			return nil, err
 		}
-
-		if headerCellCount == 0 {
-			// treat the first non-empty row as header
-			headerCellCount = len(row)
-		}
-
-		// ensure every row's length not bigger than the header's
-		if len(row) > headerCellCount {
-			row = row[:headerCellCount]
-		}
-		contents = append(contents, row)
 	}
 
-	return contents, nil
+	contents, err := r.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	header[0] = strings.TrimLeft(header[0], UTF8BOM)
+	return append([][]string{header}, contents...), nil
 }
 
 func IsSpaceField(field string) bool {
